b53436a85328e07a8a7beff468e07975e3db306e
java <java@tg.com>
2020-01-14 16:19:23 +0800

    feature

--- tg-beans/src/main/java/com/tg/framework/beans/http/SimpleHttpEntityBuilder.java
+++ tg-beans/src/main/java/com/tg/framework/beans/http/SimpleHttpEntityBuilder.java
@@ -1,37 +1,53 @@
 package com.tg.framework.beans.http;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 @SuppressWarnings("unchecked")
 public abstract class SimpleHttpEntityBuilder<T extends SimpleHttpEntityBuilder> {
 
-  protected MultiValueMap<String, String> headers;
+  protected List<NameValuePair> headers;
   protected String body;
 
-  public T headers(MultiValueMap<String, String> headers) {
+  private List<NameValuePair> requireHeaders() {
+    if (headers == null) {
+      synchronized (this) {
+        if (headers == null) {
+          headers = new ArrayList<>();
+        }
+      }
+    }
+    return headers;
+  }
+
+  public T headers(List<NameValuePair> headers) {
     this.headers = headers;
     return (T) this;
   }
 
 
-  public T addHeader(String key, String value) {
+  public T addHeader(String name, String value) {
+    requireHeaders().add(new NameValuePair(name, value));
     return (T) this;
   }
 
-  public T addHeader(String key, List<String> values) {
+  public T addHeader(String name, List<String> values) {
+    Optional.ofNullable(values).ifPresent(vs -> {
+      List<NameValuePair> headers = requireHeaders();
+      vs.forEach(v -> headers.add(new NameValuePair(name, v)));
+    });
     return (T) this;
   }
 
-  public T addHeader(MultiValueMap<String, String> values) {
+  public T addHeader(List<NameValuePair> values) {
+    Optional.ofNullable(values).ifPresent(vs -> requireHeaders().addAll(vs));
     return (T) this;
   }
 
   public T setHeader(String key, String value) {
-    return (T) this;
-  }
-
-  public T setHeader(Map<String, String> values) {
+    requireHeaders().removeIf(p -> p.getName().equals(key));
+    requireHeaders().add(new NameValuePair(key, value));
     return (T) this;
   }
 
