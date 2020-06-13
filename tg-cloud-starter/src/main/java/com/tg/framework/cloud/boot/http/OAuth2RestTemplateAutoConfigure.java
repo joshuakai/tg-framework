5847afb34bc55e58819c180624dd7b40243feacb
flower <flower@123.abc>
2020-06-14 03:47:27 +0800

    feature

--- tg-cloud-starter/src/main/java/com/tg/framework/cloud/boot/http/OAuth2RestTemplateAutoConfigure.java
+++ tg-cloud-starter/src/main/java/com/tg/framework/cloud/boot/http/OAuth2RestTemplateAutoConfigure.java
@@ -6,6 +6,7 @@ import org.apache.http.client.HttpClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,13 +20,21 @@ import org.springframework.security.oauth2.client.token.grant.client.ClientCrede
 @Import(HttpClientAutoConfigure.class)
 @ConditionalOnClass({OAuth2RestTemplate.class, HttpClient.class, LoadBalanced.class})
 @ConditionalOnProperty(prefix = "tg.cloud.oauth2-rest-template", value = "enabled")
+@ConfigurationProperties(prefix = "security.oauth2.client")
 public class OAuth2RestTemplateAutoConfigure {
+
+  private String accessTokenUri;
+  private String clientId;
+  private String clientSecret;
 
   @Bean("oAuth2RestTemplate")
   @LoadBalanced
   @ConditionalOnMissingBean(name = "oAuth2RestTemplate")
-  public OAuth2RestTemplate oAuth2RestTemplate(ClientCredentialsResourceDetails details,
-      HttpClient httpClient) {
+  public OAuth2RestTemplate oAuth2RestTemplate(HttpClient httpClient) {
+    ClientCredentialsResourceDetails details = new ClientCredentialsResourceDetails();
+    details.setAccessTokenUri(accessTokenUri);
+    details.setClientId(clientId);
+    details.setClientSecret(clientSecret);
     OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(details);
     oAuth2RestTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
     oAuth2RestTemplate.getMessageConverters().stream()
@@ -35,4 +44,27 @@ public class OAuth2RestTemplateAutoConfigure {
     return oAuth2RestTemplate;
   }
 
+  public String getAccessTokenUri() {
+    return accessTokenUri;
+  }
+
+  public void setAccessTokenUri(String accessTokenUri) {
+    this.accessTokenUri = accessTokenUri;
+  }
+
+  public String getClientId() {
+    return clientId;
+  }
+
+  public void setClientId(String clientId) {
+    this.clientId = clientId;
+  }
+
+  public String getClientSecret() {
+    return clientSecret;
+  }
+
+  public void setClientSecret(String clientSecret) {
+    this.clientSecret = clientSecret;
+  }
 }
